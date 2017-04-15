fc5432826f2d52ef1b94d62e0fd76a5fff72d9cf
Tyler Zhang <TZTylerzhang@gmail.com>
2017-04-15 15:45:12 -0400

    Prettified code

--- server/src/scorer.ts
+++ server/src/scorer.ts
@@ -1,86 +1,86 @@
 import * as mongodb from "mongodb";
 import * as bunyan from "bunyan";
-import {Scraper, PersonObject} from "./scraper";
+import { Scraper, PersonObject } from "./scraper";
 const config = require("../config.js");
 let mongoCli = mongodb.MongoClient;
 
-export default class ScoreResolver{
-    
-    rateTbl: mongodb.Collection;
-    uniTbl: mongodb.Collection;
-    voidTbl: mongodb.Collection;
-    log: bunyan;
-    constructor(log:bunyan){
-        this.log = log;
+export default class ScoreResolver {
 
-        mongoCli.connect(`mongodb://${config.dbAuth.url}:27017/RMPforQuest`, (err, d) => {
-            if(err)
-                log.error(err);
-            else {
-                this.rateTbl = d.collection("ratings");
-                this.uniTbl = d.collection("university");
-                this.voidTbl = d.collection("void");
-                log.info("Connected to mongodb");
-            }
-        });
-    }
+  rateTbl: mongodb.Collection;
+  uniTbl: mongodb.Collection;
+  voidTbl: mongodb.Collection;
+  log: bunyan;
+  constructor(log: bunyan) {
+    this.log = log;
 
-    getScore(university: string, names: string[]):Promise<returnedQuery[]|null>{
-        names = names.map(v => v.toLowerCase());
-        
-        return this.uniTbl.findOne({name: university}).then(r => {
-            if(r == null)
-                return null;
-            return this.rateTbl.find({university, name: {$in: names}}).toArray().then((docs: DatabasePerson[]) => {
-                if(docs.length == names.length){
-                    return docs.map(v => Object.assign({}, {data: v}, {queryName: v.name}));
-                } else {
-                    return this.voidTbl.find({university, name: {$in: names}}).toArray().then(voidDocs => {
-                        let totalDocs = voidDocs.concat(docs);
-                        let remainingNames = names.filter(item => !totalDocs.some(found => found.name == item));
+    mongoCli.connect(`mongodb://${config.dbAuth.url}:27017/RMPforQuest`, (err, d) => {
+      if (err)
+        log.error(err);
+      else {
+        this.rateTbl = d.collection("ratings");
+        this.uniTbl = d.collection("university");
+        this.voidTbl = d.collection("void");
+        log.info("Connected to mongodb");
+      }
+    });
+  }
 
-                        if(remainingNames.length >= config.scrapeLimit){
-                            throw new Error("Trying to query way too many people");
-                        }
+  getScore(university: string, names: string[]): Promise<returnedQuery[] | null> {
+    names = names.map(v => v.toLowerCase());
 
-                        let formatedVoidDocs = voidDocs.map(v => {return {queryName: v.name, data: null}});
-                        return Promise.all(remainingNames.map(v => this.rmpGet(v, new Scraper(university), university)))
-                        .then((remain:any) => {
-                            return docs.map(v => Object.assign({}, {data: v}, {queryName: v.name})).concat(remain).concat(formatedVoidDocs);
-                        });
-                    });
-                }
-            });
-        });
-    }
-    rmpGet(name: string, scraper: Scraper, university: string):Promise<returnedQuery>{
-        return scraper.getDataByName(name).then(d => {
-            if(d == null){
-                this.voidTbl.insertOne({university, name});
-                return null;
+    return this.uniTbl.findOne({ name: university }).then(r => {
+      if (r == null)
+        return null;
+      return this.rateTbl.find({ university, name: { $in: names } }).toArray().then((docs: DatabasePerson[]) => {
+        if (docs.length == names.length) {
+          return docs.map(v => Object.assign({}, { data: v }, { queryName: v.name }));
+        } else {
+          return this.voidTbl.find({ university, name: { $in: names } }).toArray().then(voidDocs => {
+            let totalDocs = voidDocs.concat(docs);
+            let remainingNames = names.filter(item => !totalDocs.some(found => found.name == item));
+
+            if (remainingNames.length >= config.scrapeLimit) {
+              throw new Error("Trying to query way too many people");
             }
-            else{
-                let insertObj = Object.assign({}, d, {name});
-                this.rateTbl.insertOne(insertObj);
-                return d;
-            }
-        }).catch(e => {
-            this.log.error(e);
-            return null;
-        }).then(data => {
-            return {
-                queryName: name,
-                data
-            }
-        });
-    }
+
+            let formatedVoidDocs = voidDocs.map(v => { return { queryName: v.name, data: null } });
+            return Promise.all(remainingNames.map(v => this.rmpGet(v, new Scraper(university), university)))
+              .then((remain: any) => {
+                return docs.map(v => Object.assign({}, { data: v }, { queryName: v.name })).concat(remain).concat(formatedVoidDocs);
+              });
+          });
+        }
+      });
+    });
+  }
+  rmpGet(name: string, scraper: Scraper, university: string): Promise<returnedQuery> {
+    return scraper.getDataByName(name).then(d => {
+      if (d == null) {
+        this.voidTbl.insertOne({ university, name });
+        return null;
+      }
+      else {
+        let insertObj = Object.assign({}, d, { name });
+        this.rateTbl.insertOne(insertObj);
+        return d;
+      }
+    }).catch(e => {
+      this.log.error(e);
+      return null;
+    }).then(data => {
+      return {
+        queryName: name,
+        data
+      }
+    });
+  }
 }
 
-interface DatabasePerson extends PersonObject{
-    name: string;
+interface DatabasePerson extends PersonObject {
+  name: string;
 }
 
-interface returnedQuery{
-    queryName: string;
-    data: PersonObject;
+interface returnedQuery {
+  queryName: string;
+  data: PersonObject;
 }
--- server/src/scraper.ts
+++ server/src/scraper.ts
@@ -5,97 +5,97 @@ import fetch from 'node-fetch';
 const baseUrl = "https://www.ratemyprofessors.com"
 const queryUrl = "https://www.ratemyprofessors.com/search.jsp?queryBy=teacherName";
 
-export class Scraper{
-    uniUrl: string;
+export class Scraper {
+  uniUrl: string;
 
-    constructor(school: string){
-        this.uniUrl = queryUrl + "&schoolName=" + encodeURIComponent(school);
+  constructor(school: string) {
+    this.uniUrl = queryUrl + "&schoolName=" + encodeURIComponent(school);
+  }
+
+  /**
+   * Scrapes data by the name of the professor
+   */
+  getDataByName(name: string): Promise<PersonObject> {
+    let webUrl = this.uniUrl + "&query=" + encodeURIComponent(name);
+    return fetch(webUrl).then(d => d.text())
+      .then(d => {
+        let $ = cheerio.load(d);
+        let profUrl = $("li.listing.PROFESSOR").find("a").attr("href");
+        if (!profUrl)
+          return null;
+        else
+          return fetch(baseUrl + profUrl).then(r => r.text()).then(this.scrapeData);
+      }).catch(e => {
+        console.log(e);
+        return null;
+      })
+  }
+
+  /**
+   * Scrapes data based on the id of the professor
+   */
+  getDataById(id: number): Promise<PersonObject> {
+    let url = "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + id;
+    return fetch(url).then(d => d.text())
+      .then(this.scrapeData)
+      .catch(e => {
+        console.log(e);
+        return null;
+      })
+  }
+
+  /**
+   * Scrapes data from a specific link
+   */
+  getDataByLink(url: string): Promise<PersonObject> {
+    return fetch(url).then(d => d.text())
+      .then(this.scrapeData)
+      .catch(e => {
+        console.log(e);
+        return null;
+      })
+  }
+
+  private scrapeData(html: string): PersonObject {
+    let $ = cheerio.load(html);
+
+    /** One of those weird professors where no information is found */
+    if ($(".dosanddonts").length != 0) {
+      return null;
     }
-    
-    /**
-     * Scrapes data by the name of the professor
-     */
-    getDataByName(name: string): Promise<PersonObject>{
-        let webUrl = this.uniUrl + "&query=" + encodeURIComponent(name);
-        return fetch(webUrl).then(d =>  d.text())
-        .then(d => {
-            let $ = cheerio.load(d);
-            let profUrl = $("li.listing.PROFESSOR").find("a").attr("href");
-            if(!profUrl)
-                return null;
-            else
-                return fetch(baseUrl + profUrl).then(r => r.text()).then(this.scrapeData);
-        }).catch(e => {
-            console.log(e);
-            return null;
-        })
+
+    let scoreWrapper = $(".breakdown-wrapper");
+    let quality = scoreWrapper.find("div.breakdown-container").find("div.grade").html().trim();
+
+    let breakdownSection = scoreWrapper.find(".breakdown-section");
+    let retake = breakdownSection.eq(0).children().html().trim();
+    let difficulty = breakdownSection.eq(1).children().html().trim();
+    let chilli = breakdownSection.eq(2).find("img").attr("src");
+    let ratingCount = $(".table-toggle.rating-count.active").html().trim();
+    let nameWrapper = $(".profname").children();
+    let university = $(".school").html();
+
+    let department = $(".result-title").clone().children().remove().end().text().trim();
+
+    let url = $("meta[property='og:url']").attr("content");
+
+    let rtnObj: PersonObject = {
+      fname: nameWrapper.eq(0).html().trim(),
+      mname: nameWrapper.eq(1).html().trim(),
+      lname: nameWrapper.eq(2).html().trim(),
+      quality: Number(quality),
+      retake,
+      easiness: Number(difficulty),
+      chilli,
+      count: extractRatingAmount(ratingCount),
+      url,
+      id: extractIdFromUrl(url),
+      university,
+      department: extractDepartment(department)
     }
 
-    /**
-     * Scrapes data based on the id of the professor
-     */
-    getDataById(id: number): Promise<PersonObject>{
-        let url = "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + id;
-        return fetch(url).then(d => d.text())
-        .then(this.scrapeData)
-        .catch(e => {
-            console.log(e);
-            return null;
-        })  
-    }
-
-    /**
-     * Scrapes data from a specific link
-     */
-    getDataByLink(url: string): Promise<PersonObject>{
-        return fetch(url).then(d => d.text())
-        .then(this.scrapeData)
-        .catch(e => {
-            console.log(e);
-            return null;
-        })
-    }
-
-    private scrapeData(html:string): PersonObject{
-        let $ = cheerio.load(html);
-
-         /** One of those weird professors where no information is found */
-        if($(".dosanddonts").length != 0){
-            return null;
-        }
-
-        let scoreWrapper = $(".breakdown-wrapper");
-        let quality = scoreWrapper.find("div.breakdown-container").find("div.grade").html().trim();
-
-        let breakdownSection = scoreWrapper.find(".breakdown-section");
-        let retake = breakdownSection.eq(0).children().html().trim();
-        let difficulty = breakdownSection.eq(1).children().html().trim();
-        let chilli = breakdownSection.eq(2).find("img").attr("src");
-        let ratingCount = $(".table-toggle.rating-count.active").html().trim();
-        let nameWrapper = $(".profname").children();
-        let university = $(".school").html();
-
-        let department = $(".result-title").clone().children().remove().end().text().trim();
-
-        let url = $("meta[property='og:url']").attr("content");
-
-        let rtnObj:PersonObject = {
-            fname: nameWrapper.eq(0).html().trim(),
-            mname: nameWrapper.eq(1).html().trim(),
-            lname: nameWrapper.eq(2).html().trim(),
-            quality: Number(quality),
-            retake,
-            easiness: Number(difficulty),
-            chilli,
-            count: extractRatingAmount(ratingCount),
-            url,
-            id: extractIdFromUrl(url),
-            university,
-            department: extractDepartment(department)
-        }
-
-        return rtnObj;
-    }
+    return rtnObj;
+  }
 }
 
 
@@ -104,13 +104,13 @@ let ratingRegex = /^\d+/;
  * extracts the amount of ratings from a string
  * eg. extracts 18 from "18 Student Ratings"
  */
-function extractRatingAmount(rating:string): number{
-    let result = rating.match(ratingRegex);
-    if(!result || result.length != 1){
-        console.log("malformed rating");
-        return null;
-    }else
-        return Number(result[0]);
+function extractRatingAmount(rating: string): number {
+  let result = rating.match(ratingRegex);
+  if (!result || result.length != 1) {
+    console.log("malformed rating");
+    return null;
+  } else
+    return Number(result[0]);
 }
 
 
@@ -118,13 +118,13 @@ let idRegex = /ShowRatings.jsp\?tid=(\d+)$/;
 /**
  * extracts the id number of the professor from the url on the page
  */
-function extractIdFromUrl(url: string): number{
-    let result = url.match(idRegex);
-    if(!result || result.length < 2){
-        console.log("malformed id");
-        return null;
-    }else
-        return Number(result[1]);
+function extractIdFromUrl(url: string): number {
+  let result = url.match(idRegex);
+  if (!result || result.length < 2) {
+    console.log("malformed id");
+    return null;
+  } else
+    return Number(result[1]);
 }
 
 
@@ -132,32 +132,32 @@ let departmentRegex = /Professor in the (.+) department/i;
 /**
  * extracts the department of the professor from the description
  */
-function extractDepartment(title: string): string{
-    let result = title.match(departmentRegex);
-    if(!result || result.length < 2){
-        console.log("malformed description");
-        return null;    
-    }
-    else
-        return result[1];
+function extractDepartment(title: string): string {
+  let result = title.match(departmentRegex);
+  if (!result || result.length < 2) {
+    console.log("malformed description");
+    return null;
+  }
+  else
+    return result[1];
 }
 
 /**
  * The data object returned by the scraper
  */
-export interface PersonObject{
-    quality: number;
-    retake: string;
-    easiness: number;
-    chilli: string,
-    count: number;
-    fname: string;
-    lname: string;
-    mname: string;
-    id: number;
-    url: string;
-    university: string;
-    department: string;
+export interface PersonObject {
+  quality: number;
+  retake: string;
+  easiness: number;
+  chilli: string,
+  count: number;
+  fname: string;
+  lname: string;
+  mname: string;
+  id: number;
+  url: string;
+  university: string;
+  department: string;
 }
 /*
 let testScraper = new Scraper("University of Waterloo");
