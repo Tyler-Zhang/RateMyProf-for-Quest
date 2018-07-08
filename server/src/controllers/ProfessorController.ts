26a15e32071c9188d6d6b3d4aa08fc859632a3cc
Tyler Zhang <tztylerzhang@gmail.com>
2018-07-08 02:31:25 -0400

    remove unneeded dependencies

--- server/src/controllers/ProfessorController.ts
+++ server/src/controllers/ProfessorController.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Get, Post, UseAfter } from "routing-controllers";
+import { Controller, Body, Post } from "routing-controllers";
 import { ProfessorReviewRequest } from "../requests";
 import { ProfessorReviewService } from "../services/ProfessorReviewService";
 import { Inject } from "typedi";
